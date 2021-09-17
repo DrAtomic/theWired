af2a487707acade164f9494cf96a2b8ddef576fb
DrAtomic <leo.ward.garcia@gmail.com>
2021-09-16 21:58:49 -0700

    got user input

--- curses.c
+++ curses.c
@@ -1,15 +1,21 @@
 #include <ncurses.h>
-#include <stdlib.h>
+#include <string.h>
 
-void login(){
-  addstr("plz enter credetnials\n");
-  refresh();
-  getchar();
-};
 
 int main(){
+	char mesg[] = "Enter Username: ";
+	char str[80];
+	int row, col;
+
+
 	initscr();
-	login();
+	getmaxyx(stdscr,row,col);
+	mvprintw(row/2,(col-strlen(mesg))/2,"%s",mesg);
+
+	getstr(str);
+	mvprintw(LINES - 2, 0, "You Entered: %s", str);
+	getch();
 	endwin();
+
 	return 0;
 };
